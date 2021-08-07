aec81ef375a9e05199c4e1e9ccfe8bdf0c8df3b2
Sirzes <sirzes02@gmail.com>
2021-08-06 21:23:31 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import LoginButton from "./components/LoginButton";
-import Profile from "./components/Profile";
-import LogoutButton from "./components/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./app.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
 const App = () => {
   const { isAuthenticated, isLoading } = useAuth0();
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 import JSONPretty from "react-json-pretty";
 import "react-json-pretty/themes/monikai.css";
 
